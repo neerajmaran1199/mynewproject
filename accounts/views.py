226f2e1e7f98f841f425388cf9fa6c3be7aabb2c
neeraj <neerajmaran1199@gmail.com>
2020-04-25 08:38:23 +0530

    added readme

---
+++ accounts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.contrib.auth.forms import UserCreationForm
+
+
+class register( UserCreationForm):
+
+    pass
--- project_1/urls.py
+++ project_1/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('newspaper.urls'))
+    path('', include('newspaper.urls')),
+    path('accounts/', include('accounts.urls'))
 ]
 
 if settings.DEBUG:
